b5d6b1a43b5e65fd6bd0fc149debf5e2e1d9369b
OJ Abba <phavorabba@gmail.com>
2025-04-29 19:21:35 +0100

    [Bug] Fix entity name in `getLinkedCustomerIds`

--- src/api/deskpro/getLinkedCustomerIds.ts
+++ src/api/deskpro/getLinkedCustomerIds.ts
@@ -1,3 +1,4 @@
+import { ENTITY_NAME } from "@/constants";
 import { IDeskproClient } from "@deskpro/app-sdk";
 
 export default async function getLinkedCustomerIds(client?: IDeskproClient, userId?: string): Promise<string[]> {
@@ -5,6 +6,6 @@ export default async function getLinkedCustomerIds(client?: IDeskproClient, user
         return []
     }
 
-    const linkedIds = await client.getEntityAssociation("linkedSmartsheetTasks", userId).list()
+    const linkedIds = await client.getEntityAssociation(ENTITY_NAME, userId).list()
     return linkedIds
 }
