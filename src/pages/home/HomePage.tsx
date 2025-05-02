4b939f422f0e00ee96f47c8994dac0cfffa4d2fa
tidbit1 <194808961+tidbit1@users.noreply.github.com>
2025-05-02 12:28:48 +0100

    develop home-page with minimal details of linked customer

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,6 +1,26 @@
-import { useDeskproElements } from '@deskpro/app-sdk';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Link, LoadingSpinner, useDeskproElements, useDeskproLatestAppContext, useInitialisedDeskproAppClient } from '@deskpro/app-sdk';
+import Container from '@/components/Container/Container';
+import ErrorBlock from '@/components/ErrorBlock';
+import Title from '@/components/Title/Title';
+import QuickBooksLogo from '@/components/QuickBooksLogo';
+import TextBlockWithLabel from '@/components/TextBlockWithLabel/TextBlockWithLabel';
+import { getCustomerByEmail } from '@/api/quickbooks';
+import { ContextData, ContextSettings } from '@/types/deskpro';
+import { QuickBooksCustomer } from '@/types/quickbooks';
 
 function HomePage() {
+    const { context } = useDeskproLatestAppContext<ContextData, ContextSettings>();
+    const navigate = useNavigate();
+    const [customer, setCustomer] = useState<QuickBooksCustomer>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+
+    useInitialisedDeskproAppClient(client => {
+        client.setTitle('Home');
+    });
+
     useDeskproElements(({ clearElements, registerElement }) => {
         clearElements();
         registerElement('refresh', { type: 'refresh_button' });
@@ -17,9 +37,78 @@ function HomePage() {
         });
     }, []);
 
-    return (
-        <h1>Home</h1>
+    useInitialisedDeskproAppClient(async client => {
+        if (!context) {
+            return;
+        };
+
+        try {
+            setIsLoading(true);
+
+            const customer = await getCustomerByEmail(client, {
+                companyId: context.settings.company_id,
+                email: context.data?.user?.primaryEmail || '',
+            });
+
+            customer && setCustomer(customer);
+        } catch (error) {
+            setError(`error finding linked customer: ${error}`);
+        } finally {
+            setIsLoading(false);
+        };
+    }, [context]);
+
+    const ItemTitle = ({ title }: {title: string}) => (
+        <Link
+            href='#'
+            onClick={event => {
+                event.preventDefault();
+                navigate('/customers/view');
+            }}
+        >
+            {title}
+        </Link>
     );
+
+    if (isLoading) {
+        return <LoadingSpinner />
+    };
+
+    if (error) {
+        return (
+            <Container>
+                <ErrorBlock>{error}</ErrorBlock>
+            </Container>
+        );
+    };
+
+    if (!customer) {
+        return (
+            <Container>
+                <p>No Linked Customer</p>
+            </Container>
+        );
+    };
+
+    if (customer) {
+        return (
+            <Container>
+                <Title
+                    title={<ItemTitle title={customer.DisplayName} />}
+                    icon={<QuickBooksLogo />}
+                    link={`https://qbo.intuit.com/app/customerdetail?nameId=${customer.Id}`}
+                />
+                <TextBlockWithLabel
+                    label='Name'
+                    text={`${customer.GivenName} ${customer.FamilyName}`}
+                />
+                <TextBlockWithLabel
+                    label='Email'
+                    text={customer.PrimaryEmailAddr.Address}
+                />
+            </Container>
+        );
+    };
 };
 
 export default HomePage;
