9839da1bff61158418b0bd1a137c9beca24d37bb
OJ Abba <61709964+Nugwxa@users.noreply.github.com>
2025-06-12 16:36:24 +0100

    [Bug] Fix App Requiring Users Re-Authenticate After Successful Login (SC-190943) (#47)
    * Fixes a bug in  the `baseRequest` function resulting in the initial request failing and always requiring a refresh to succeed.
    * Improves the error message being shown to the user when on the login page with custom messages for multiple scenarios.
    * Prevents refreshing the access token when using the global proxy ("Quick Connect").
    * Fixes the conditional rendering logic in `manifest.json`, adding better support for `undefined`.

--- src/pages/login/useLogin.ts
+++ src/pages/login/useLogin.ts
@@ -25,20 +25,24 @@ export default function useLogin(): UseLoginResult {
     const { context } = useDeskproLatestAppContext<ContextData, ContextSettings>()
 
     const user = context?.data?.user
+    const mode = context?.settings.use_advanced_connect === false ? 'global' : 'local';
+    const isUsingSandbox = context?.settings.use_sandbox === true
+    const settings = context?.settings
 
     // @todo: Update useInitialisedDeskproAppClient typing in the
     // App SDK to to properly handle both async and sync functions
 
     // eslint-disable-next-line @typescript-eslint/no-misused-promises
     useInitialisedDeskproAppClient(async client => {
-        if (!context?.settings || !user) {
+        if (!settings || !user) {
             return;
         };
 
-        const clientID = context.settings.client_id;
-        const mode = context.settings.use_advanced_connect ? 'local' : 'global';
+        const clientId = settings.client_id;
 
-        if (mode === 'local' && typeof clientID !== 'string') {
+        if (mode === 'local' && (typeof clientId !== 'string' || clientId.trim() === "")) {
+            // Local mode requires a clientId.
+            setError("No client id was provided while setting up the app, a client id is required when using advanced connect.")
             return;
         };
 
@@ -47,7 +51,7 @@ export default function useLogin(): UseLoginResult {
             ({ state, callbackUrl }) => {
                 return `https://appcenter.intuit.com/connect/oauth2?${createSearchParams([
                     ["response_type", "code"],
-                    ["client_id", clientID || ''],
+                    ["client_id", clientId ?? ''],
                     ["redirect_uri", callbackUrl],
                     ['scope', SCOPE],
                     ["state", state],
@@ -71,11 +75,11 @@ export default function useLogin(): UseLoginResult {
 
         setAuthUrl(oauth2Response.authorizationUrl)
         setOAuth2Context(oauth2Response)
-    }, [context, setAuthUrl]);
+    }, [settings, setAuthUrl, mode, user]);
 
 
     useInitialisedDeskproAppClient((client) => {
-        if (!user || !oauth2Context) {
+        if (!user || !oauth2Context || !settings?.company_id) {
             return
         }
 
@@ -90,19 +94,37 @@ export default function useLogin(): UseLoginResult {
                 }
 
                 try {
-                    await getCompanyInfo(client, context.settings.company_id);
+                    await getCompanyInfo(client, settings.company_id);
                     void navigate('/');
                 } catch (error) {
                     if (error instanceof QuickBooksError && isQuickBooksFaultError(error.data)) {
                         const fault = error.data.Fault ?? error.data.fault;
 
-                        if (fault?.type === "AuthorizationFault") {
-                            throw new Error("The user logging in isn't a part of the company specified in the app setup. Contact your admin for more information");
+                        switch (fault?.type) {
+                            case "AUTHENTICATION":
+                                throw new Error("An error occurred while authenticating the user.")
+                            case "AuthorizationFault":
+                                throw new Error("The user logging in isn't a part of the company specified in the app setup. Contact your admin for more information.");
+                            case "AuthenticationFault":
+                                if (fault.Error?.[0].Message === "Accessing Wrong Cluster") {
+                                    const errorMessage = mode === "global" ?
+                                        "Error authenticating user: Sandbox accounts cannot be used with Quick Connect."
+                                        :
+                                        `Error authenticating user: Ensure the QuickBooks app is setup to use ${isUsingSandbox ? "sandbox" : "production"} accounts.`
+                                    throw new Error(errorMessage)
+                                }
+                                break
+                            case "SERVICE":
+                                if (fault.error?.[0].code === "3100") {
+                                    throw new Error(`Error authenticating user: Ensure the QuickBooks app is setup to use ${isUsingSandbox ? "sandbox" : "production"} accounts.`)
+
+                                }
+                                break
                         }
                     };
 
                     // generic error for all other errors
-                    throw new Error('error authenticating user. Are you using a sandbox company (double-check on QuickBooks, and the settings drawer of the QuickBooks app in Deskpro)?');
+                    throw new Error(`An unexpected error occurred: ${error instanceof Error ? error.message : "Unknown Error"}.`);
                 };
             } catch (error) {
                 setError(error instanceof Error ? error.message : 'unknown error');
@@ -115,7 +137,7 @@ export default function useLogin(): UseLoginResult {
         if (isPolling) {
             void startPolling()
         }
-    }, [isPolling, user, oauth2Context, navigate, context?.settings.company_id]);
+    }, [isPolling, user, oauth2Context, navigate, settings?.company_id]);
 
     const onSignIn = useCallback(() => {
         setIsLoading(true);
